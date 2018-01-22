0d3a3cec4a6cf1f5adc78947aa6f61d5aa808d68
Michael Bradley <michaelbradleyuk2001@hotmail.com>
2018-01-22 21:56:57 +0000

    Create MimeHelper.cs

---
+++ MimeHelper.cs
@@ -0,0 +1,5 @@
+public static class MimeHelper
+{
+  public static string Pdf => "application/pdf";
+  public static string Excel => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+}
